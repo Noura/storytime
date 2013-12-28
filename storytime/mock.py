d57d87faa5148301eea06f2844f89513061a2085
Noura <culurienneldoreth@gmail.com>
2013-12-28 00:18:04 -0500

    reformatted page JSON a little bit

--- storytime/mock.py
+++ storytime/mock.py
@@ -1,12 +1,11 @@
 MOCK_STORY = [
 	{'title': "Jesod's Magical Adventure",
-	'pageText': ['Pork belly voluptate aute bespoke, Tumblr deserunt drinking vinegar selfies selvage placeat keffiyeh consectetur. Adipisicing mumblecore Intelligentsia reprehenderit id tofu. Sapiente Austin cardigan, cred butcher Portland ennui disrupt tofu master cleanse 90\'s seitan. Intelligentsia master cleanse beard, Etsy stumptown duis aute dolore Cosby sweater farm-to-table Vice High Life viral Echo Park Tumblr. YOLO forage High Life Odd Future typewriter. Chillwave chia fugiat consectetur, brunch incididunt et shabby chic letterpress quinoa Schlitz. Labore Intelligentsia single-origin coffee Odd Future.',
+	'text': ['Pork belly voluptate aute bespoke, Tumblr deserunt drinking vinegar selfies selvage placeat keffiyeh consectetur. Adipisicing mumblecore Intelligentsia reprehenderit id tofu. Sapiente Austin cardigan, cred butcher Portland ennui disrupt tofu master cleanse 90\'s seitan. Intelligentsia master cleanse beard, Etsy stumptown duis aute dolore Cosby sweater farm-to-table Vice High Life viral Echo Park Tumblr. YOLO forage High Life Odd Future typewriter. Chillwave chia fugiat consectetur, brunch incididunt et shabby chic letterpress quinoa Schlitz. Labore Intelligentsia single-origin coffee Odd Future.',
 'Exercitation photo booth locavore street art raw denim VHS. Bespoke cornhole pork belly artisan, literally banh mi lo-fi master cleanse brunch laborum hoodie craft beer. Keffiyeh four loko brunch put a bird on it culpa, pour-over High Life pork belly mumblecore voluptate VHS exercitation. Bushwick veniam cliche hella seitan Godard. Lo-fi pariatur et, banh mi cornhole Marfa farm-to-table ex skateboard shabby chic chambray quinoa sustainable. Artisan odio enim velit whatever single-origin coffee. Irony blog hoodie forage, yr nisi Cosby sweater skateboard laborum gluten-free VHS cliche labore.',
 'Truffaut Cosby sweater master cleanse Williamsburg. Distillery whatever church-key mustache tempor sartorial. American Apparel authentic put a bird on it, commodo non selvage butcher sartorial squid ethical duis gastropub flannel. Sriracha occaecat bespoke lomo small batch. Plaid Echo Park sustainable, bespoke narwhal locavore Shoreditch semiotics. Etsy sapiente try-hard literally small batch yr. Photo booth biodiesel laborum sapiente Bushwick synth dolore, High Life et consectetur brunch gluten-free placeat.',
 'Disrupt enim ugh, in readymade letterpress narwhal Terry Richardson quis. Anim flannel plaid bicycle rights ex DIY. Fanny pack messenger bag Bushwick, farm-to-table vero Austin forage narwhal. American Apparel put a bird on it Thundercats jean shorts ethical. Irure High Life synth ullamco kogi freegan. Slow-carb twee exercitation, messenger bag chia shabby chic occaecat 90\'s sunt pariatur scenester gluten-free. Incididunt duis tousled fap Wes Anderson, enim ullamco Carles occaecat shabby chic gastropub retro literally.'],
 	'image': '/img/jesod.jpg',
-	'meta': {'author' : 'Dr. Traze',
-				'pageNumber' : 1
-			}
+	'author' : 'Dr. Traze',
+	'pageNumber' : 1,
 	}
 ]
