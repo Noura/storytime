421a886333e6db73fec77e4d7d18fe9bc19bb250
Noura <culurienneldoreth@gmail.com>
2013-12-28 22:13:33 -0500

    can go forward or backward, slightly buggy

--- angular-storytime/app/js/directives.js
+++ angular-storytime/app/js/directives.js
@@ -1,17 +1,26 @@
 'use strict';
 
+// myTextFit
+// displays text word by word so it fills up the page without overflowing
 storytime.directive('myTextFit', function($document) {
-
     function link(scope, element, attrs) {
 
-      var display_page = function() {
+      scope.start_p = 0; // index in scope.paragraphs of first word on page
+      scope.start_w = 0; // index in scope.paragraphs[scope.start_p] of
+                         // first word on page
+      scope.end_p = 0;   // index in scope.paragraphs of last word on page
+      scope.end_w = 0;   // index in scope.paragraphs[scope.end_p] of
+                         // last word on page
+
+      // TODO combine display_page_forward and display_page_backward into
+      // one function
+
+      // TODO sometimes words get lost when changing pages. fix these off by one errors
+      var display_page_forward = function() {
+        console.log('display_page_forward with scope', scope);
         var $inner = $('<div></div>');
         var $last_word = null;
         element.html($inner);
-        if (scope.start_p == undefined) {
-          scope.start_p = 0;
-          scope.start_w = 0;
-        }
         var p = scope.start_p;
         var w = scope.start_w;
         do {
@@ -32,16 +41,70 @@ storytime.directive('myTextFit', function($document) {
           $last_word.remove();
         }
         scope.end_p = p;
-        scope.end_w = w + 1;
-      }
+        scope.end_w = Math.max(w-1, 0);
+      };
 
-      display_page();
+      var display_page_backward = function() {
+        console.log('display_page_backward with scope', scope);
+        var $inner = $('<div></div>');
+        var $first_word = null;
+        element.html($inner);
+        var p = scope.end_p;
+        var w = scope.end_w - 1;
+        if (w < 0) {
+          w = 0;
+          p -= 1;
+        }
+        if (p < 0) {
+          return;
+        }
+        do {
+          if (p >= 0) {
+            if (w > 0) {
+              $first_word = $('<span> '+scope.paragraphs[p][w]+'</span>');
+              w -= 1;
+            } else {
+              p -= 1;
+              if (p < 0) {
+                break;
+              }
+              w = scope.paragraphs[p].length -1;
+              $first_word = $('<br><br>');
+            }
+            $inner.prepend($first_word);
+          }
+        } while (   $inner.height() < element.height()
+                 && p >= 0 );
+        if ($first_word) {
+          $first_word.remove();
+        }
+        scope.start_p = p;
+        scope.start_w = w;
+      };
 
+      display_page_forward(); // display the first page upon initialization
+
+      // listen for PageCtrl.nextPage() with the scope.go_to_next_page flag
       scope.$watch(function() {
-        return scope.start_p;
+        return scope.go_to_next_page;
       }, function(newValue, oldValue) {
-        if (newValue !== oldValue) {
-          display_page();
+        if (scope.go_to_next_page === true) {
+          scope.start_w = scope.end_w;
+          scope.start_p = scope.end_p;
+          display_page_forward();
+          scope.go_to_next_page = false;
+        }
+      });
+
+      // listen for PageCtrl.prevPage() with the scope.go_to_prev_page flag
+      scope.$watch(function() {
+        return scope.go_to_prev_page;
+      }, function(newValue, oldValue) {
+        if (scope.go_to_prev_page === true) {
+          scope.end_w = scope.start_w;
+          scope.end_p = scope.start_p;
+          display_page_backward();
+          scope.go_to_prev_page = false;
         }
       });
     }
@@ -49,6 +112,10 @@ storytime.directive('myTextFit', function($document) {
     return {
       link: link
     };
+
+  // myPageTurner
+  // draws the page turning arrows at the corners of the book image
+  // updates position as book image resizes with window
   }).directive('myPageTurner', function($document) {
     function link(scope, element, attrs) {
       var $window = $(window);
