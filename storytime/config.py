d4082becb10cdb09089fffc601a765caa6eba51d
George Sass <george.a.sass@gmail.com>
2013-12-28 21:00:30 -0500

    Added configurations for SQLAlchemy and secret code (for POST data/auth encryption).
    TODO: Add paths for other resources.

--- storytime/config.py
+++ storytime/config.py
@@ -1,1 +1,16 @@
-#TODO - configs for default routes, crypto stuff, etc.
+from random import randint
+
+def genSecretKey(length):
+	return ''.join([chr(randint(0,255)) for i in range(25)])
+
+class config(object):
+	DEBUG = False
+	TESTING = False
+	DB_ROOT = ''
+	SECRET_KEY = genSecretKey(25)
+
+class debugConfig(config):
+	DEBUG = True
+	TESTING = True
+	#Use sqlite for testing purposes
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/storytime.db'
