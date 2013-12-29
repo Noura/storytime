66d1688ba84ba3f1b1b2bc12229c80abe25e551d
George Sass <george.a.sass@gmail.com>
2013-12-28 22:05:31 -0500

    Added options to the app manage.
    App manager now supports configuration of the app, and db_init.

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-from storytime import app
+from storytime import app, db
 import storytime.config as config
 
 from flask.ext.script import Manager
@@ -7,14 +7,23 @@ from flask.ext.script import Manager
 manager = Manager(app)
 
 @manager.command
-def production():
-    app.config.from_object(config.productionConfig)
-    app.run()
+def init_db(config=None):
+    configure()
+    db.create_all()
 
 @manager.command
-def debug():
-    app.config.from_object(config.debugConfig)
+def run(config=None):
+    configure()
     app.run()
 
+def configure(config=None):
+    if config=='debug':
+        app.config.from_object(config.debugConfig)
+    elif config=='production':
+        app.config.from_object(config.productionConfig)
+    else:
+        raise NameError("This configuration name is not allowed!")
+
 if __name__=='__main__':
+    manager.add_option('-c', '--config', dest='config', required=True)
     manager.run()
