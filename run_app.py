7052bb0c887185fe5f86d2a83faf17ca8bc9486b
George Sass <george.a.sass@gmail.com>
2013-12-28 23:29:25 -0500

    Fixed manager config options in run_app.

--- run_app.py
+++ run_app.py
@@ -4,26 +4,25 @@ import storytime.config as config
 from flask.ext.script import Manager
 
 
-manager = Manager(app)
+def create_app(cfg=None):
+    if cfg=='debug':
+        app.config.from_object(config.debugConfig)
+    elif cfg=='production':
+        app.config.from_object(config.productionConfig)
+    else:
+        raise NameError("This cfguration name is not allowed!")
+    return app
+
+manager = Manager(create_app)
 
 @manager.command
-def init_db(config=None):
-    configure()
+def init_db():
     db.create_all()
 
 @manager.command
-def run(config=None):
-    configure()
+def run():
     app.run()
 
-def configure(config=None):
-    if config=='debug':
-        app.config.from_object(config.debugConfig)
-    elif config=='production':
-        app.config.from_object(config.productionConfig)
-    else:
-        raise NameError("This configuration name is not allowed!")
-
 if __name__=='__main__':
-    manager.add_option('-c', '--config', dest='config', required=True)
+    manager.add_option('-c', '--config', dest='cfg', required=False)
     manager.run()
